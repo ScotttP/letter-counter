29d7600b58041880b3470944011e1f1a91e7e003
Scott Pribe <Scottpribe@gmail.com>
2020-04-10 12:23:14 -0400

    JS letter counter and reset button functional.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const input = document.querySelector('#inputBox');
 const letterCount = document.querySelector('#letterCount');
 const clearCount = document.querySelector('#clearCount');
+
+document.getElementById('letterCount').innerText = "Letter Count: " + `${finalLetterCount = 0}`;
+
+clearCount.addEventListener('click', () => {
+    clear();
+})
+
+function strCount (strChar) {
+    stringLetters= strChar.replace(/[^a-zA-Z]/g, "");
+    finalLetterCount = stringLetters.length
+    document.getElementById('letterCount').innerText = "Letter Count: " + `${finalLetterCount}`;
+}
+
+function clear (){
+    document.getElementById('inputBox').value = "";
+    document.getElementById('letterCount').innerText = "Letter Count: " + `${finalLetterCount = 0 }`;
+} 
